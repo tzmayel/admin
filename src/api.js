70ce15ac9b62f3ea0d353604e4acf09f7df01e9f
Tom Mayel (mac) <tom@mayel.net>
2020-04-13 03:03:22 +0100

    prettier fixes

--- src/api.js
+++ src/api.js
@@ -1,34 +1,35 @@
 const loadApiData = (page, recordsPerPage, callback) => {
+  const sendDate = new Date().getTime();
+  let responseTimeMs = 0;
+  fetch(`http://localhost:3001/api/subscribers/${page}/${recordsPerPage}`)
+    .then((res) => {
+      const receiveDate = new Date().getTime();
+      responseTimeMs = receiveDate - sendDate;
 
-    const sendDate = (new Date()).getTime();
-    let responseTimeMs = 0;
-    fetch(`http://localhost:3001/api/subscribers/${page}/${recordsPerPage}`)
-      .then(res => {
-        const receiveDate = (new Date()).getTime();
-        responseTimeMs = receiveDate - sendDate;
+      if (res.status === 200) {
+        return res.json();
+      }
 
-        if (res.status === 404) {
-            callback({
-            rowData: null,
-            responseTime: responseTimeMs
-          })
-        }
-        else {
-          return res.json();
-        }
-      })
-      .then(res => {
+      callback({
+        rowData: null,
+        responseTime: responseTimeMs,
+      });
+      return null;
+    })
+    .then((res) => {
+      if (res) {
         callback({
           rowData: res.records,
           totalDbRecords: res.debugInfo.totalRecordsCount,
-          responseTime: responseTimeMs
-        })
-      })
-      .catch(e => {
-        console.log('e=', e);
-      });
-  }
+          responseTime: responseTimeMs,
+        });
+      }
+    })
+    .catch(() => {
+      // console.log('e=', e);
+    });
+};
 
-  export default {
-      loadApiData
-  }
+export default {
+  loadApiData,
+};
